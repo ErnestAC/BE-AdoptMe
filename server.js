afd1b9566c941f16979379531b313592276d6b28
Ernest <rabergcobo@gmail.com>
2025-05-14 18:15:59 -0700

    Clustered

---
+++ server.js
@@ -0,0 +1,25 @@
+// server.js
+import cluster from 'cluster';
+import os from 'os';
+import logger from './src/utils/logger.js';
+import app from './src/app.js';
+
+const PORT = process.env.PORT || 9988;
+const numCPUs = os.cpus().length;
+
+if (cluster.isPrimary) {
+    logger.info(`Primary process ${process.pid} is running`);
+
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        logger.warn(`Worker ${worker.process.pid} died. Starting a new one...`);
+        cluster.fork();
+    });
+} else {
+    app.listen(PORT, () => {
+        logger.info(`Worker ${process.pid} started on port ${PORT}`);
+    });
+}
--- src/app.js
+++ src/app.js
@@ -1,44 +1,39 @@
+// app.js
 import express from 'express';
-import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
-
-// custom error handler
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import { errorHandler } from './middlewares/errorHandler.js';
-
-// custom console.log
 import logger from './utils/logger.js';
-
 
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
 import loggerTestRouter from './routes/loggerTest.router.js';
-import dotenv from 'dotenv';
+
 dotenv.config({ path: `.env.${process.env.NODE_ENV || 'dev'}` });
 
 mongoose.set('strictQuery', false);
 const MONGO_URI = process.env.MONGO_URI;
+mongoose.connect(MONGO_URI).catch(err => logger.error("MongoDB connection error:", err));
 
 const app = express();
-const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(MONGO_URI)
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/users',usersRouter);
-app.use('/api/pets',petsRouter);
-app.use('/api/adoptions',adoptionsRouter);
-app.use('/api/sessions',sessionsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/pets', petsRouter);
+app.use('/api/adoptions', adoptionsRouter);
+app.use('/api/sessions', sessionsRouter);
 app.use('/', loggerTestRouter);
 
-//testing for error handler
+// Error test route
 app.get('/test-error', (req, res) => {
     throw new Error('Test error for logging');
 });
 
-app.listen(PORT,()=>logger.info(`Listening on ${PORT}`))
-
-//error logger
 app.use(errorHandler);
+
+export default app;
